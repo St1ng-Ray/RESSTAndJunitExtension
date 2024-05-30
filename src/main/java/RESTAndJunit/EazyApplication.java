855531224b9748e9a8c2bb585eda6fd22ba1b249
Arisen <minakamiyuki200128@gmail.com>
2024-05-30 14:36:31 +0800

    取消SpringSecurity的自动配置

--- src/main/java/RESTAndJunit/EazyApplication.java
+++ src/main/java/RESTAndJunit/EazyApplication.java
@@ -2,11 +2,12 @@ package RESTAndJunit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @RestController
 public class EazyApplication {
     public static void main(String[] args) {
