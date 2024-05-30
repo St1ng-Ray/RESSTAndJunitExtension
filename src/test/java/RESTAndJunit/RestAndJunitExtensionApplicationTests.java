4914ebacb08c5bbc58124eb8360becefee8f3dcf
Arisen <minakamiyuki200128@gmail.com>
2024-05-30 10:15:37 +0800

    Let's rock

---
+++ src/test/java/RESTAndJunit/RestAndJunitExtensionApplicationTests.java
@@ -0,0 +1,13 @@
+package RESTAndJunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAndJunitExtensionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
